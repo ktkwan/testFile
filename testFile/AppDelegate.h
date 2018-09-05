c01f3075489bb41ce0779aaf8fe5017b64f5ae46
ktkwan <katiekwan@katies-mbp.devices.brown.edu>
2018-09-04 22:54:46 -0400

    Initial Commit

---
+++ testFile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testFile
+//
+//  Created by Katie Kwan on 9/4/18.
+//  Copyright © 2018 Katie Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testFile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testFile
+//
+//  Created by Katie Kwan on 9/4/18.
+//  Copyright © 2018 Katie Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
